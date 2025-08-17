e075f710bf0fbc489b05c4ac9d177c5583ecf613
R-lth <bluearth0502@gmail.com>
2025-08-17 10:54:14 +0900

    :white_check_mark: 한글 주석 깨짐 테스트
    각 소스 선택 > 파일 > 다른 이름으로 저장 > 인코딩 : 유니코드, 서명 있는 UTF-8로 설정
    
    → .git의 config나 .editorconfig 설정으로 해결되지 않음
    → 각 소스의 인코딩 변경 후 해결됨

--- D11/D11/pch.h
+++ D11/D11/pch.h
@@ -2,8 +2,8 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
-// ����� ����� ���, DirectXTex ���̺귯���� ����� ���� ���
-// ������ ����� ���, �Ϲ� DirectXTex ���̺귯�� ���
+// 디버그 빌드일 경우, DirectXTex 라이브러리의 디버그 버전 사용
+// 릴리즈 빌드일 경우, 일반 DirectXTex 라이브러리 사용
 #ifdef _DEBUG
 #pragma comment(lib, "DirectXTex\\DirectXTex_Debug.lib")
 #else
