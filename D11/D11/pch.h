407fbc75ecb326aa3c094eb7fa885bc6e76100ac
R-lth <bluearth0502@gmail.com>
2025-08-17 10:55:49 +0900

    pch.h 및 관련 헤더 추가

--- D11/D11/pch.h
+++ D11/D11/pch.h
@@ -1,4 +1,31 @@
-// DirectX ���̺귯�� ��ũ
+﻿#pragma once
+
+#include "Types.h"
+#include "values.h"
+#include "Struct.h"
+
+// STL
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+// WIN
+#include <windows.h>
+#include <assert.h>
+
+// DIRECTX
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+#include <DirectXTex/DirectXTex.h>
+#include <DirectXTex/DirectXTex.inl>
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+// DirectX 라이브러리 링크
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
---
+++ D11/D11/values.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#include "Types.h"
+
+const int32 GWindSizeX = 800;
+const int32 GWindSizeY = 600;
