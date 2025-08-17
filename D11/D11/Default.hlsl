6206b4302a020d9c4422440826eab619420c9369
R-lth <bluearth0502@gmail.com>
2025-08-17 16:52:26 +0900

    렌더링파이프 라인 이해 및 VertexShader.hlsl 구현
    [삼각형 띄우기] (26:32)
    
    [hlsl 속성]
    셰이더 모델 : 5.0
    진입점 이름 : VS // main 제거 후 VS로 지정
    
    [hlsl 코드]
    렌더링 파이프라인 셰이더 관련

--- D11/D11/Default.hlsl
+++ D11/D11/Default.hlsl
@@ -1,4 +1,44 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+﻿
+struct VS_INPUT
 {
-	return pos;
+    float4 position : POSITION; // 정점 좌표
+    float4 color : COLOR;       // 정점 색상
+};
+
+struct VS_OUTPUT
+{
+    float4 position : SV_POSITION; // 시스템 예약 시맨틱(화면 위치용)
+    float4 color : COLOR;          // 픽셀 셰이더로 넘길 색상
+};
+
+// IA - VS(정점 단위) - RS(자동/보간) - PS - OM
+/*
+ * IA (정점 데이터)
+   ↓
+   VS (정점 셰이더: 좌표 변환, 데이터 전달)
+   ↓
+   RS (래스터라이저: 정점 → 픽셀)
+   ↓
+   PS (픽셀 셰이더: 최종 색상 계산)
+   ↓
+   OM (출력 병합기: 화면 버퍼에 출력)
+*/
+
+//
+// 렌더링 파이프라인의 VS 단계
+VS_OUTPUT VS(VS_INPUT input)
+{
+    VS_OUTPUT output;
+    output.position = input.position;
+    output.color = input.color;
+    
+    return output;
+}
+
+// RS 자동
+
+// 렌더링 파이프라인의 PS 단계
+float4 PS(VS_OUTPUT input) : SV_Target
+{
+    return float4(1, 0, 0, 0); // 항상 빨강(R=1, G=0, B=0, A=0)
 }
