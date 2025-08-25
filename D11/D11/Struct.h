1e84d495cda93e5ef41b0ed7c413de01df2f27d4
R-lth <bluearth0502@gmail.com>
2025-08-25 19:35:36 +0900

    :tada: 프로젝트 재시작. 데스크탑 애플리케이션(.exe)

--- D11/D11/Struct.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "Types.h"
-
-struct Vertex
-{
-	Vec3 position; // 0번 offset 
-	Color color;   // 12번 offset
-};
--- DX11/Client/Client.cpp
+++ DX11/Client/Client.cpp
@@ -1,64 +1,63 @@
-﻿// D11.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
 
 #include "pch.h"
 #include "framework.h"
-#include "D11.h"
-#include "Game.h"
+#include "Client.h"
 
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
-HWND hWnd;
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    // 1. 윈도우 창 정보 등록
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_CLIENT, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
-    // 2. 윈도우 창 생성. 애플리케이션 초기화를 수행합니다:
+    // 애플리케이션 초기화를 수행합니다:
     if (!InitInstance (hInstance, nCmdShow))
     {
         return FALSE;
     }
 
-    Game game;
-    // 1) 초기화
-    game.Init(hWnd);
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CLIENT));
 
-    MSG msg = {};
+    MSG msg;
 
     // 기본 메시지 루프입니다:
-    //while (GetMessage(&msg, nullptr, 0, 0))
-    while (msg.message != WM_QUIT)
+    while (GetMessage(&msg, nullptr, 0, 0))
     {
-        //if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
-        }
-        // 게임 프레임워크 실행
-        else 
-        {
-            // 2) 매 프레임마다
-            game.Update();
-            game.Render();
         }
     }
 
     return (int) msg.wParam;
 }
 
-#pragma region Win32 Window 
+
+
 //
 //  함수: MyRegisterClass()
 //
@@ -75,11 +74,11 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_D11));
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CLIENT));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = NULL;
-    wcex.lpszClassName  = L"Dx11 Practice";
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CLIENT);
+    wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
@@ -99,22 +98,16 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-    // 창 크기 설정
-    RECT windowRect = { 0, 0, GWinSizeX, GWinSizeY }; // 800, 600
-    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, false);
-
-    // 창 생성
-    hWnd = CreateWindowW(L"Dx11 Practice", L"Kohmeso, DXClient", WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, 0, windowRect.right - windowRect.left,windowRect.bottom - windowRect.top, nullptr, nullptr, hInstance, nullptr);
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
       return FALSE;
    }
 
-    // 표준으로 제공하는 winAPI임을 기록
-    ::ShowWindow(hWnd, nCmdShow);
-    ::UpdateWindow(hWnd);
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
 
    return TRUE;
 }
@@ -139,9 +132,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // 메뉴 선택을 구문 분석합니다:
             switch (wmId)
             {
-                /*case IDM_ABOUT:
+            case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;*/
+                break;
             case IDM_EXIT:
                 DestroyWindow(hWnd);
                 break;
@@ -166,7 +159,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     }
     return 0;
 }
-#pragma endregion
 
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
 
-
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
