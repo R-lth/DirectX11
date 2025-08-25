2e7e9b20bedc0cf1b18919fb3effe9ea3ac162d2
R-lth <bluearth0502@gmail.com>
2025-08-25 21:10:23 +0900

    인코딩 변경

--- DX11/Client/Client.cpp
+++ DX11/Client/Client.cpp
@@ -1,4 +1,4 @@
-// Client.cpp : ���ø����̼ǿ� ���� �������� �����մϴ�.
+﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
 #include "pch.h"
@@ -7,12 +7,12 @@
 
 #define MAX_LOADSTRING 100
 
-// ���� ����:
-HINSTANCE hInst;                                // ���� �ν��Ͻ��Դϴ�.
-WCHAR szTitle[MAX_LOADSTRING];                  // ���� ǥ���� �ؽ�Ʈ�Դϴ�.
-WCHAR szWindowClass[MAX_LOADSTRING];            // �⺻ â Ŭ���� �̸��Դϴ�.
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
-// �� �ڵ� ��⿡ ���Ե� �Լ��� ������ �����մϴ�:
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -26,12 +26,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: ���⿡ �ڵ带 �Է��մϴ�.
+    // TODO: 여기에 코드를 입력합니다.
 
-    // ���� ���ڿ��� �ʱ�ȭ�մϴ�.
+    // 전역 문자열을 초기화합니다.
     MyRegisterClass(hInstance);
 
-    // ���ø����̼� �ʱ�ȭ�� �����մϴ�:
+    // 애플리케이션 초기화를 수행합니다:
     if (!InitInstance(hInstance, nCmdShow))
     {
         return FALSE;
@@ -39,7 +39,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg = {};
 
-    // �⺻ �޽��� �����Դϴ�:
+    // 기본 메시지 루프입니다:
     while (msg.message != WM_QUIT)
     {
         if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
@@ -55,9 +55,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 //
-//  �Լ�: MyRegisterClass()
+//  함수: MyRegisterClass()
 //
-//  �뵵: â Ŭ������ ����մϴ�.
+//  용도: 창 클래스를 등록합니다.
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
@@ -81,18 +81,18 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 }
 
 //
-//   �Լ�: InitInstance(HINSTANCE, int)
+//   함수: InitInstance(HINSTANCE, int)
 //
-//   �뵵: �ν��Ͻ� �ڵ��� �����ϰ� �� â�� ����ϴ�.
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
 //
-//   �ּ�:
+//   주석:
 //
-//        �� �Լ��� ���� �ν��Ͻ� �ڵ��� ���� ������ �����ϰ�
-//        �� ���α׷� â�� ���� ���� ǥ���մϴ�.
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-    hInst = hInstance; // �ν��Ͻ� �ڵ��� ���� ������ �����մϴ�.
+    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
     HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
@@ -109,13 +109,13 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 }
 
 //
-//  �Լ�: WndProc(HWND, UINT, WPARAM, LPARAM)
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
-//  �뵵: �� â�� �޽����� ó���մϴ�.
+//  용도: 주 창의 메시지를 처리합니다.
 //
-//  WM_COMMAND  - ���ø����̼� �޴��� ó���մϴ�.
-//  WM_PAINT    - �� â�� �׸��ϴ�.
-//  WM_DESTROY  - ���� �޽����� �Խ��ϰ� ��ȯ�մϴ�.
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
 //
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -126,7 +126,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
-        // TODO: ���⿡ hdc�� ����ϴ� �׸��� �ڵ带 �߰��մϴ�...
+        // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
         EndPaint(hWnd, &ps);
     }
     break;
@@ -137,24 +137,4 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
-}
-
-// ���� ��ȭ ������ �޽��� ó�����Դϴ�.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
